5a7872c3e9587ae786875ce92ee4f9f79df77afe
kevin <kevinmartinezcl13@gmail.com>
2026-08-26 11:01:06 -0500

    ci: habilitar triggers en ramas feature

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Conexión exitosa a la base de datos"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050, debug=False)
