6882b48c61f3a1eba0e288fd3bd21b3ff1e91e72
kevin <kevinmartinezcl13@gmail.com>
2026-08-26 10:51:11 -0500

    Test: Inyectar fallos intencionales para Bandit, Trivy y Pytest

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+MYSQL_PASSWORD = "super_secret_123"
+
 @app.route('/')
 def home():
     return "Conexión exitosa a la base de datos"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050, debug=True)
